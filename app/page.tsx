d9f621141315911d7ef1e1391f1b117d42320322
KasunShakthi <2001shakthikasun@gmail.com>
2025-02-18 19:49:26 +0530

    Initial Development

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>This is the Testing Project</h1>;
+  return <h1>This is the Development branch Testing Project</h1>;
 }
